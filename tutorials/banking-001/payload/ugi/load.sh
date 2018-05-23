3f1e2dc84ecb524fca18fc37a312a5a54b864a57
sachin prasad <sachin.prasad1@ibm.com>
2018-05-23 16:07:07 -0700

    restructuring

---
+++ tutorials/banking-001/payload/ugi/load.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+basedir=$(pwd)
+
+
+## Import Categories, Terms, Policy and Rules
+CLI_DIR=/opt/IBM/InformationServer/Clients/istools/cli/
+
+${CLI_DIR}/istool.sh glossary import -dom is-servicesdocker:9446 -u isadmin -p isadmin -filename "${basedir}/data/sample-business-glossary-xml-export.xml" -format XML -mergemethod overwrite
+
